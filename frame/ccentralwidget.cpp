89eaa4a0f642e14b955e5989b9ea79d0349e6b39
Xin Wang <xin.wang@deepin.io>
2019-09-05 15:24:03 +0800

    feat: add scroll bar
    set scroll bar auto show

--- frame/ccentralwidget.cpp
+++ frame/ccentralwidget.cpp
@@ -108,6 +108,10 @@ void CCentralwidget::initUI()
     m_pGraphicsView->setAlignment(Qt::AlignCenter);
     m_pGraphicsView->setRenderHint(QPainter::Antialiasing);//设置反走样
 
+    //自动设置滚动条
+    m_pGraphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    m_pGraphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
     m_progressLayout = new ProgressLayout();
 
 
