6a16f8c7f31274124cbd03ff0b675f191b722388
Zheng Youge <youge.zheng@deepin.io>
2019-09-12 21:52:01 +0800

    chore:fix theme config file location

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ static QString g_appPath;//全局路径
 DGuiApplicationHelper::ColorType getThemeTypeSetting()
 {
     //需要找到自己程序的配置文件路径，并读取配置，这里只是用home路径下themeType.cfg文件举例,具体配置文件根据自身项目情况
-    QString t_appDir = g_appPath + QDir::separator() + ".themetype.cfg";
+    QString t_appDir = g_appPath + QDir::separator() + "themetype.cfg";
     QFile t_configFile(t_appDir);
 
     t_configFile.open(QIODevice::ReadOnly | QIODevice::Text);
@@ -50,7 +50,7 @@ DGuiApplicationHelper::ColorType getThemeTypeSetting()
 void saveThemeTypeSetting(int type)
 {
     //需要找到自己程序的配置文件路径，并写入配置，这里只是用home路径下themeType.cfg文件举例,具体配置文件根据自身项目情况
-    QString t_appDir = g_appPath + QDir::separator() + ".themetype.cfg";
+    QString t_appDir = g_appPath + QDir::separator() + "themetype.cfg";
     QFile t_configFile(t_appDir);
 
     t_configFile.open(QIODevice::WriteOnly | QIODevice::Text);
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
     a.setApplicationVersion(DApplication::buildVersion(t_date));
 
     //主题设置
-    g_appPath = QDir::homePath() + QDir::separator() + qApp->applicationName();
+    g_appPath = QDir::homePath() + QDir::separator() + "." + qApp->applicationName();
     QDir t_appDir;
     t_appDir.mkpath(g_appPath);
 
