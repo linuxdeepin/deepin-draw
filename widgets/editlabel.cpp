0f8ca8f7f2f12ec84c9d8f1404aeb027d5af96e5
jiangchangli <changli.jiang@deepin.io>
2019-10-30 23:41:15 +0800

    fix:color laber read only
    set the color label read only

--- widgets/editlabel.cpp
+++ widgets/editlabel.cpp
@@ -35,7 +35,8 @@ EditLabel::EditLabel(DWidget *parent)
     m_edit = new DLineEdit(this);
     m_edit->setFixedSize(LINEEDIT_SIZE);
     m_edit->setClearButtonEnabled(false);
-    m_edit->lineEdit()->setEnabled(false);
+    //m_edit->lineEdit()->setEnabled(false);
+    m_edit->lineEdit()->setReadOnly(true);
 
     QFont font = m_edit->font();
     font.setPixelSize(8);
