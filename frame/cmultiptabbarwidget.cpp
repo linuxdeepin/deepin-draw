dd7b3b82a56cc20a7504d287c44fd58f72b90cee
zhanghao <zhanghao@uniontech.com>
2020-03-24 09:31:46 +0800

    Title:chore edit multip tab file
    Description:修改多标签文件逻辑
    
    RootCause:Null
    
    Solution:修改多标签文件逻辑

--- frame/cmultiptabbarwidget.cpp
+++ frame/cmultiptabbarwidget.cpp
@@ -54,8 +54,8 @@ CMultipTabBarWidget::~CMultipTabBarWidget()
 
 void CMultipTabBarWidget::closeTabBarItem(QString itemName)
 {
-    for (int i=0; i < this->count(); i++) {
-        if(itemName == this->tabText(i)){
+    for (int i = 0; i < this->count(); i++) {
+        if (itemName == this->tabText(i)) {
             this->removeTab(i);
         }
     }
@@ -68,7 +68,7 @@ void CMultipTabBarWidget::setDefaultTabBarName(QString name)
 
 void CMultipTabBarWidget::addTabBarItem(QString name)
 {
-    if(name.isEmpty()){
+    if (name.isEmpty()) {
         emit this->tabAddRequested();
         return;
     }
@@ -78,8 +78,8 @@ void CMultipTabBarWidget::addTabBarItem(QString name)
     emit signalNewAddItem(name);
     updateTabWidth();
 
-    for(int i = 0; i < this->count(); i++){
-        if(name == this->tabText(i)){
+    for (int i = 0; i < this->count(); i++) {
+        if (name == this->tabText(i)) {
             this->setCurrentIndex(i);
         }
     }
@@ -106,13 +106,13 @@ void CMultipTabBarWidget::updateTabWidth()
 void CMultipTabBarWidget::initConnection()
 {
     // [0] 连接点击 + 按钮响应事件
-    connect(this,&DTabBar::tabAddRequested,this,[=](){
+    connect(this, &DTabBar::tabAddRequested, this, [ = ]() {
         // 组装标签名字
         m_nameCounter++;
         QString divName = m_defaultName + QString::number(m_nameCounter);
 
         // 如果已经存在标签则显示存在的标签
-        if(tabBarNameIsExist(divName)){
+        if (tabBarNameIsExist(divName)) {
             m_nameCounter--;
             return ;
         }
@@ -121,13 +121,13 @@ void CMultipTabBarWidget::initConnection()
     });
 
     // [1] 连接点击关闭响应事件
-    connect(this,&DTabBar::tabCloseRequested,this,[=](int index){
+    connect(this, &DTabBar::tabCloseRequested, this, [ = ](int index) {
         QString closeItemName = this->tabText(index);
         emit signalTabItemCloseRequested(closeItemName);
     });
 
     // [2] 连接标签选择改变响应事件
-    connect(this,&DTabBar::currentChanged,this,[=](int index){
+    connect(this, &DTabBar::currentChanged, this, [ = ](int index) {
         QString currentName = this->tabText(index);
         emit signalItemChanged(currentName);
     });
@@ -138,12 +138,12 @@ bool CMultipTabBarWidget::tabBarNameIsExist(QString name)
     for (int i = 0; i < this->count(); i++) {
         QString tempName = this->tabText(i);
         // 此判断用于文件是否已经修改
-        if(tempName.startsWith("* ") && tempName.endsWith(" ")) {
-            tempName = tempName.replace(0,2,"");
-            tempName = tempName.replace(tempName.lastIndexOf(" "),1,"");
+        if (tempName.startsWith("* ") && tempName.endsWith(" ")) {
+            tempName = tempName.replace(0, 2, "");
+            tempName = tempName.replace(tempName.lastIndexOf(" "), 1, "");
         }
 
-        if(tempName == name) {
+        if (tempName == name) {
             this->setCurrentIndex(i);
             return true;
         }
@@ -154,8 +154,8 @@ bool CMultipTabBarWidget::tabBarNameIsExist(QString name)
 void CMultipTabBarWidget::updateTabBarName(QString oldName, QString newName)
 {
     for (int i = 0; i < this->count(); i++) {
-        if(this->tabText(i)==oldName){
-            this->setTabText(i,newName);
+        if (this->tabText(i) == oldName) {
+            this->setTabText(i, newName);
             return;
         }
     }
@@ -171,22 +171,22 @@ QString CMultipTabBarWidget::getNextTabBarDefaultName()
 
 void CMultipTabBarWidget::setTabBarTooltipName(QString tabName, QString tooltip)
 {
-    for(int i = 0; i < this->count(); i++){
-        if(tabName == this->tabText(i)){
-            setTabBarTooltipName(static_cast<quint16>(i),tooltip);
+    for (int i = 0; i < this->count(); i++) {
+        if (tabName == this->tabText(i)) {
+            setTabBarTooltipName(static_cast<quint16>(i), tooltip);
         }
     }
 }
 
 void CMultipTabBarWidget::setTabBarTooltipName(quint16 index, QString tooltip)
 {
-    this->setTabToolTip(index,tooltip);
+    this->setTabToolTip(index, tooltip);
 }
 
 void CMultipTabBarWidget::setCurrentTabBarWithName(QString tabName)
 {
-    for(int i = 0; i < this->count(); i++){
-        if(tabName == this->tabText(i)){
+    for (int i = 0; i < this->count(); i++) {
+        if (tabName == this->tabText(i)) {
             this->setCurrentIndex(i);
         }
     }
@@ -197,62 +197,63 @@ void CMultipTabBarWidget::resizeEvent(QResizeEvent *event)
     DTabBar::resizeEvent(event);
     updateTabWidth();
     this->update();
-    this->resize(width(),36);
+    this->resize(width(), 36);
 }
 
 bool CMultipTabBarWidget::eventFilter(QObject *, QEvent *event)
 {
     if (event->type() == QEvent::MouseButtonPress) {
-            QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
 
-            if (mouseEvent->button() == Qt::RightButton) {
-                QPoint position = mouseEvent->pos();
-                m_rightClickTab = -1;
+        if (mouseEvent->button() == Qt::RightButton) {
+            QPoint position = mouseEvent->pos();
+            m_rightClickTab = -1;
 
-                // 判断当前是否找到该标签
-                for (int i = 0; i < count(); i++) {
-                    if (this->tabRect(i).contains(position)) {
-                        m_rightClickTab = i;
-                        break;
-                    }
-                }
-
-                // popup right menu on tab.
-                if (m_rightClickTab >= 0) {
-                    m_rightMenu = new DMenu;
-
-                    m_closeTabAction = new QAction(tr("Close tab"), this);
-                    m_closeOtherTabAction = new QAction(tr("Close other tabs"), this);
-
-                    connect(m_closeTabAction, &QAction::triggered, this, [=] {
-                        // 跳转到需要关闭的标签
-                        this->setCurrentIndex(m_rightClickTab);
-
-                        emit signalTabItemCloseRequested(this->tabText(m_rightClickTab));
-                    });
-
-                    connect(m_closeOtherTabAction, &QAction::triggered, this, [=] {
-                        QStringList closeNames;
-                        for(int i = 0; i < this->count(); i++) {
-                            if(m_rightClickTab == i){
-                                continue;
-                            }
-                            closeNames << this->tabText(i);
-                        }
-                        emit signalTabItemsCloseRequested(closeNames);
-                    });
-
-                    m_rightMenu->addAction(m_closeTabAction);
-                    m_rightMenu->addAction(m_closeOtherTabAction);
-
-                    m_rightMenu->exec(mapToGlobal(position));
-
-                    return true;
+            // 判断当前是否找到该标签
+            for (int i = 0; i < count(); i++) {
+                if (this->tabRect(i).contains(position)) {
+                    m_rightClickTab = i;
+                    break;
                 }
             }
-        } else {
-            event->accept();
+
+            // popup right menu on tab.
+            if (m_rightClickTab >= 0) {
+                m_rightMenu = new DMenu;
+
+                m_closeTabAction = new QAction(tr("Close tab"), this);
+                m_closeOtherTabAction = new QAction(tr("Close other tabs"), this);
+
+                connect(m_closeTabAction, &QAction::triggered, this, [ = ] {
+                    // 跳转到需要关闭的标签
+                    this->setCurrentIndex(m_rightClickTab);
+
+                    emit signalTabItemCloseRequested(this->tabText(m_rightClickTab));
+                });
+
+                connect(m_closeOtherTabAction, &QAction::triggered, this, [ = ] {
+                    QStringList closeNames;
+                    for (int i = 0; i < this->count(); i++)
+                    {
+                        if (m_rightClickTab == i) {
+                            continue;
+                        }
+                        closeNames << this->tabText(i);
+                    }
+                    emit signalTabItemsCloseRequested(closeNames);
+                });
+
+                m_rightMenu->addAction(m_closeTabAction);
+                m_rightMenu->addAction(m_closeOtherTabAction);
+
+                m_rightMenu->exec(mapToGlobal(position));
+
+                return true;
+            }
         }
-        return false;
+    } else {
+        event->accept();
+    }
+    return false;
 }
 
