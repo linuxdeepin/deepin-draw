fbfc86f2e837bedd7e9521f079a2c1e288157e9d
zouya <zouya@uniontech.com>
2020-03-31 13:22:24 +0800

    Title:feat Cut and paste multiple entities and place them at the top according to the original layer order
    Description:任务号17667 剪切后粘贴图元，改变图元顺序到顶层，多个图元剪切后粘贴，按照原图层顺序置顶；
    
    RootCause:Null
    
    Solution:

--- frame/cgraphicsview.cpp
+++ frame/cgraphicsview.cpp
@@ -901,6 +901,8 @@ void CGraphicsView::slotOnDelete()
             allItems.push_back(curSeleteItems.first());
         }
     }
+    //升序排列
+    qSort(allItems.begin(), allItems.end(), zValueSortASC);
 
     QUndoCommand *deleteCommand = new CDeleteShapeCommand(curScene, allItems);
     m_pUndoStack->push(deleteCommand);
