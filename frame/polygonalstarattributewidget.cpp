d779729b23d98bbf5511ff2c8a69ebbb58c5f712
zhanghao <zhanghao@uniontech.com>
2020-04-02 09:56:01 +0800

    Title:fix start anchor number show error
    Description:18866 星形的半径框里百分数存在显示不全情况
    
    RootCause:Null
    
    Solution:解决 星形的半径框里百分数存在显示不全情况

--- frame/polygonalstarattributewidget.cpp
+++ frame/polygonalstarattributewidget.cpp
@@ -168,8 +168,8 @@ void PolygonalStarAttributeWidget::initUI()
     m_radiusLabel->setFont(ft1);
     m_radiusNumber = new CSpinBox(this);
     m_radiusNumber->setKeyboardTracking(false);
-    m_radiusNumber->setRange(-1, 1000);
-    m_radiusNumber->setFixedSize(QSize(70, 36));
+    m_radiusNumber->setRange(0, 1000);
+    m_radiusNumber->setFixedSize(QSize(80, 36));
     m_radiusNumber->setSuffix("%");
     m_radiusNumber->setFont(ft);
     m_radiusNumber->setSpecialValueText("— —");
