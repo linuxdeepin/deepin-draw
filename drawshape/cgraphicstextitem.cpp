6fa5fbb78d8d0ccca546ac22b14231c72806bfbb
zhanghao <zhanghao@uniontech.com>
2020-04-03 21:48:35 +0800

    Title:fix set text default color is black
    Description:19203 画版文字默认颜色应该为黑色
    
    RootCause:Null
    
    Solution:画版文字默认颜色应该为黑色

--- drawshape/cgraphicstextitem.cpp
+++ drawshape/cgraphicstextitem.cpp
@@ -299,11 +299,11 @@ void CGraphicsTextItem::paint(QPainter *painter, const QStyleOptionGraphicsItem 
         painter->setClipping(false);
         QPen pen;
         pen.setWidthF(1 / CManageViewSigleton::GetInstance()->getCurView()->getDrawParam()->getScale());
-        if ( CManageViewSigleton::GetInstance()->getThemeType() == 1) {
-            pen.setColor(QColor(224, 224, 224));
-        } else {
-            pen.setColor(QColor(69, 69, 69));
-        }
+//        if ( CManageViewSigleton::GetInstance()->getThemeType() == 1) {
+        pen.setColor(QColor(224, 224, 224));
+//        } else {
+//        pen.setColor(QColor(69, 69, 69));
+//        }
         painter->setPen(pen);
         painter->setBrush(QBrush(Qt::NoBrush));
         painter->drawRect(this->boundingRect());
