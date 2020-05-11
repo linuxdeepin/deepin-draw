a88e94319bff397412f9c25998ef15e6e7f2aafd
zouya <zouya@uniontech.com>
2020-05-11 11:50:30 +0800

    Title:fix Press and hold shift to select an element or multiple elements, and click an element and drag it to multiple elements to form multi selection status
    Description:bug 25037 【画板】【5.8.0.18】按住shift选中一个图元或多个图元，使用鼠标点击一个图元拖动到多选的图
    元后，会形成多选状态
    
    RootCause:未按住shift时点击未清除多选
    
    Solution:未按shift时，若点击多选外图元，清除多选

--- drawshape/cselecttool.cpp
+++ drawshape/cselecttool.cpp
@@ -115,6 +115,12 @@ void CSelectTool::mousePressEvent(QGraphicsSceneMouseEvent *event, CDrawScene *s
         if (ctrlKeyPress) {
             scene->clearSelection();
         }
+        if (!shiftKeyPress && m_currentSelectItem) {
+            CGraphicsItem *itemcast = dynamic_cast<CGraphicsItem *>(m_currentSelectItem);
+            if (itemcast && !scene->getItemsMgr()->getItems().contains(itemcast)) {
+                scene->getItemsMgr()->clear();
+            }
+        }
         if (altKeyPress && m_currentSelectItem) {
             CGraphicsItem *itemcast = dynamic_cast<CGraphicsItem *>(m_currentSelectItem);
             if (itemcast && !scene->getItemsMgr()->getItems().contains(itemcast)) {
