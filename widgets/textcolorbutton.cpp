6256007545886d1747817dc8e71747154ef029a6
jixianglong <jixianglong@uniontech.com>
2020-05-14 11:34:21 +0800

    Type:fix about button border color
    Description:20963 关于线条按钮边线颜色的再调整
    
    RootCause:Null
    
    Solution:线条按钮边线颜色的再调整

--- widgets/textcolorbutton.cpp
+++ widgets/textcolorbutton.cpp
@@ -110,6 +110,8 @@ void TextColorButton::mousePressEvent(QMouseEvent * )
 
 void TextColorButton::paintLookStyle(QPainter *painter, bool isMult)
 {
+    //const QColor borderColor(77, 82, 93, int(1.0 * 255));
+    const QColor borderColor(255, 255, 255, int(0.1 * 255));
     painter->save();
     painter->setRenderHints(QPainter::Antialiasing);
 
@@ -120,7 +122,7 @@ void TextColorButton::paintLookStyle(QPainter *painter, bool isMult)
     QPen pen(painter->pen());
 
     bool   darkTheme = (CManageViewSigleton::GetInstance()->getThemeType() == 2);
-    QColor penColor  = darkTheme ? QColor(77, 82, 93, int(1.0 * 255)) : QColor(0, 0, 0, int(0.1 * 255));
+    QColor penColor  = darkTheme ? borderColor : QColor(0, 0, 0, int(0.1 * 255));
 
     pen.setColor(penColor);
 
