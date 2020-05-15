76fead9dd2dea85b10dd07f79c2eab095c13c3aa
zouya <zouya@uniontech.com>
2020-05-15 17:13:22 +0800

    Title:fix Bug 25499 code fallback
    Description:bug 25499 【画板】【5.8.0.18】在画板没有填充色的时候，更改透明度，图元会添加颜色
    
    RootCause:Null
    
    Solution:代码回退

--- drawshape/cdrawparamsigleton.cpp
+++ drawshape/cdrawparamsigleton.cpp
@@ -25,6 +25,7 @@
 CDrawParamSigleton::CDrawParamSigleton(const QString &uuid, bool isModified)
     : m_nlineWidth(2)
     , m_sLineColor(Qt::black)//black
+    , m_rectXRedius(5)
     , m_radiusNum(50)
     , m_anchorNum(5)
     , m_sideNum(5)
@@ -44,15 +45,14 @@ CDrawParamSigleton::CDrawParamSigleton(const QString &uuid, bool isModified)
     , m_ddfSavePath("")
     , m_effect(MasicoEffect)
     , m_blurWidth(20)
-    , m_renderImage(0)
-    , m_bSelectAlling(false)
-    , m_rectXRedius(5)
     , m_lineStartType(ELineType::noneLine)
     , m_lineEndType(ELineType::noneLine)
     , m_penStartType(ELineType::noneLine)
     , m_penEndType(ELineType::noneLine)
+    , m_renderImage(0)
+    , m_bSelectAlling(false)
 {
-    m_nFillColor = Qt::white;
+    m_nFillColor = Qt::black;
     m_nFillColor.setAlpha(0);//transparent
     m_textFont.setPointSize(14);
     m_textFont.setPointSizeF(14);
