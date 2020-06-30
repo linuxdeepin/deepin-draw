d53a287fa4d89652a2fb079b5c748ad434ea5bed
jixianglong <jixianglong@uniontech.com>
2020-06-30 14:37:45 +0800

    feat:source pro add 'make test' command
    画板源工程添加测试命令‘make test’ 完成测试用例覆盖率报告生成。

--- tests/ci/lcov.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "start..."
-cd ../../build-tests-unknown-Debug/CMakeFiles/deepin-draw-test.dir
-lcov --directory . --zerocounters  
-./../../deepin-draw-test
-lcov --directory . --capture --output-file deepin-draw_Coverage.info
-genhtml -o ../../results deepin-draw_Coverage.info
