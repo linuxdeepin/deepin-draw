0fa9c191810d899130dc5eec2fe5248baf6d3862
zhanghao@uniontech.com <zhanghao@uniontech.com>
2020-07-21 15:26:22 +0800

    test:call test process from application
    增加测试启动程序接口测试代码

--- tests/test_application.cpp
+++ tests/test_application.cpp
@@ -18,3 +18,11 @@ TEST(isFileNameLegal, ins4)
 {
     ASSERT_EQ(false, dApp->isFileNameLegal(""));
 }
+TEST(execDraw, ins5)
+{
+    QStringList li;
+    li.append(":/test.ddf");
+    li.append(":/test.png");
+    QString path = "";
+    ASSERT_EQ(0, dApp->execDraw(li, path));
+}
