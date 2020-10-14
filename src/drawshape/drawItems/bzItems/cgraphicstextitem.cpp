a3841be4a8324cdb2e62f5d6157307f5c20fcfcb
ut000526 <zhanghao@uniontech.com>
2020-10-14 14:05:15 +0800

    fix: solve text item merge property error
    解决文字图元合并属性错误
    
    Log: 解决文字图元合并属性错误
    Change-Id: Ib88636695b088b3f46d16dc2ff66234247c8622f
    Signed-off-by: ut000526 <zhanghao@uniontech.com>
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/deepin-draw/+/7455
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: wangyu <wangyu@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/drawshape/drawItems/bzItems/cgraphicstextitem.cpp
+++ src/drawshape/drawItems/bzItems/cgraphicstextitem.cpp
@@ -322,7 +322,7 @@ void CGraphicsTextItem::setTextFontStyle(const QString &style)
     }
 
     m_pTextEdit->textCursor().beginEditBlock();
-    QTextCharFormat fmt = m_pTextEdit->currentCharFormat();
+    QTextCharFormat fmt;
     fmt.setFontWeight(weight);
     mergeFormatOnWordOrSelection(fmt);
     m_Font.setStyleName(style);// 缓存自身最新的字体样式
@@ -332,7 +332,7 @@ void CGraphicsTextItem::setTextFontStyle(const QString &style)
 void CGraphicsTextItem::setFontSize(qreal size)
 {
     m_pTextEdit->textCursor().beginEditBlock();
-    QTextCharFormat fmt = m_pTextEdit->currentCharFormat();
+    QTextCharFormat fmt;
     fmt.setFontPointSize(size);
     mergeFormatOnWordOrSelection(fmt);
     m_Font.setPointSizeF(size);
@@ -347,7 +347,7 @@ qreal CGraphicsTextItem::getFontSize()
 
 void CGraphicsTextItem::setFontFamily(const QString &family)
 {
-    QTextCharFormat fmt = m_pTextEdit->currentCharFormat();
+    QTextCharFormat fmt;
     fmt.setFontFamily(family);
     mergeFormatOnWordOrSelection(fmt);
     m_Font.setFamily(family);
@@ -392,7 +392,7 @@ void CGraphicsTextItem::setTextColor(const QColor &col)
 {
     //qDebug() << "Content: " << col;
     m_pTextEdit->textCursor().beginEditBlock();
-    QTextCharFormat fmt = m_pTextEdit->currentCharFormat();
+    QTextCharFormat fmt;
     fmt.setForeground(col);
     mergeFormatOnWordOrSelection(fmt);
     m_color = col;
@@ -406,7 +406,7 @@ QColor CGraphicsTextItem::getTextColor()
 
 void CGraphicsTextItem::setTextColorAlpha(const int &alpha)
 {
-    QTextCharFormat fmt = m_pTextEdit->currentCharFormat();
+    QTextCharFormat fmt;
     m_color.setAlpha(alpha);
     fmt.setForeground(m_color);
     mergeFormatOnWordOrSelection(fmt);
