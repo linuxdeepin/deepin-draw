9f39d21bdf917b262cafed5d7ae137a49b3f130d
jixianglong <jixianglong@uniontech.com>
2020-10-19 13:34:45 +0800

    fix: the scal button to DIconButton
    解决深色主题下缩放增加减少按钮的意外阴影问题
    
    Log: 解决深色主题下缩放增加减少按钮的意外阴影问题
    Bug: https://pms.uniontech.com/zentao/bug-view-20952.html
    Change-Id: Iba56765fb72a4f3b6f321d6327883004385c007c
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/deepin-draw/+/7906
    Reviewed-by: zhanghao <zhanghao@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: wangyu <wangyu@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/widgets/dzoommenucombobox.cpp
+++ src/widgets/dzoommenucombobox.cpp
@@ -241,8 +241,18 @@ void DZoomMenuComboBox::initUI()
     });
 
     // [1] 左右加减按钮
-    m_increaseBtn = new DFloatingButton(QIcon::fromTheme("ddc_button_add_hover"), "", this);
-    m_reduceBtn = new DFloatingButton(QIcon::fromTheme("ddc_button_reduce_hover"), "", this);
+    //m_increaseBtn = new DFloatingButton(QIcon::fromTheme("ddc_button_add_hover"), "", this);
+    //m_reduceBtn = new DFloatingButton(QIcon::fromTheme("ddc_button_reduce_hover"), "", this);
+
+    m_increaseBtn = new DIconButton(this);
+    m_reduceBtn = new DIconButton(this);
+
+    m_increaseBtn->setEnabledCircle(true);
+    m_reduceBtn->setEnabledCircle(true);
+
+    m_increaseBtn->setIcon(QIcon::fromTheme("ddc_button_add_hover"));
+    m_reduceBtn->setIcon(QIcon::fromTheme("ddc_button_reduce_hover"));
+
     m_increaseBtn->setFixedSize(QSize(m_floatingSize, m_floatingSize));
     m_reduceBtn->setFixedSize(QSize(m_floatingSize, m_floatingSize));
     m_reduceBtn->setBackgroundRole(QPalette::Button);
