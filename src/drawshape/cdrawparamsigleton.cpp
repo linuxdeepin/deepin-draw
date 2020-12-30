58b59779516bb158bcc99cc1554c6ef80e522a68
jixianglong <jixianglong@uniontech.com>
2020-12-30 21:00:39 +0800

    fix: slove the issue that property widget not show when text editor actived then clike left tool button
    解决文字编辑框处于编辑状态时到点击左边界面工具按钮可能会没有显示出属性界面的问题
    
    Log: 解决文字编辑框处于编辑状态时到点击左边界面工具按钮可能会没有显示出属性界面的问题
    Bug: https://pms.uniontech.com/zentao/bug-view-59700.html
    Change-Id: I5c9d11a2d89e300ec48b45e068421e16ca16a249

--- src/drawshape/cdrawparamsigleton.cpp
+++ src/drawshape/cdrawparamsigleton.cpp
@@ -201,8 +201,11 @@ void CDrawParamSigleton::setCurrentDrawToolMode(EDrawToolMode mode)
     }
 
     // [0] 刷新顶部菜单栏属性显示
-    if (drawApp->topToolbar() != nullptr && drawApp->topToolbar()->attributWidget() != nullptr)
-        drawApp->topToolbar()->attributWidget()->showByType(tp, pItem);
+    if (drawApp->topToolbar() != nullptr && drawApp->topToolbar()->attributWidget() != nullptr) {
+        QMetaObject::invokeMethod(drawApp, [ = ]() {
+            drawApp->topToolbar()->attributWidget()->showByType(tp, pItem);
+        }, Qt::QueuedConnection);
+    }
 
     // [1] 刷新点击工具栏后改变鼠标样式
     drawApp->currentDrawScence()->changeMouseShape(mode);
