2e697fc9532c5ba4700601a88c3fd7030c82ad6b
jixianglong <jixianglong@uniontech.com>
2021-01-14 15:30:15 +0800

    fix: init text item with eidtor hidden
    在初始化文本图元时,应该隐藏编辑框控件,这样才能保证被复制出来的图元不会强占焦点
    
    Log: 解决复制文本后可能导致的快捷键等按键失效的问题
    Change-Id: I6b12d7594e996d54e77272ca606eacec71988744

--- src/drawshape/drawItems/bzItems/cgraphicstextitem.cpp
+++ src/drawshape/drawItems/bzItems/cgraphicstextitem.cpp
@@ -83,6 +83,10 @@ void CGraphicsTextItem::initTextEditor(const QString &text)
     QTextCursor textCursor = m_pTextEdit->textCursor();
     textCursor.select(QTextCursor::Document);
     m_pTextEdit->setTextCursor(textCursor);
+
+    //初始化时必须隐藏编辑框,防止抢用焦点(如复制出文本图元时)
+    m_pTextEdit->hide();
+
     changToReadOnlyState();
     m_pTextEdit->document()->clearUndoRedoStacks();
 }
