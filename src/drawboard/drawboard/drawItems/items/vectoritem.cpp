8ffe87e87a711db3e371fae9d107dcfbf9ee0d6f
tanlang <tanlang@uniontech.com>
2022-06-21 13:08:17 +0800

    fix: 组合后属性显示问题
    修复组合后属性显示问题
    
    Bug: https://pms.uniontech.com/bug-view-142973.html
    Bug: https://pms.uniontech.com/bug-view-142927.html
    Bug: https://pms.uniontech.com/bug-view-142915.html
    Bug: https://pms.uniontech.com/bug-view-142909.html
    Log: 修复组合后属性显示问题
    Change-Id: Id7df18ba765ec04bedbcd4e6cb6a002f2fd8f582

--- src/drawboard/drawboard/drawItems/items/vectoritem.cpp
+++ src/drawboard/drawboard/drawItems/items/vectoritem.cpp
@@ -146,22 +146,6 @@ SAttrisList VectorItem::attributions()
            <<  SAttri(EEnablePenStyle,  d_VectorItem()->getDrawBorder())
            <<  SAttri(EStyleProper, QVariant());
 
-    if (!childPageItems().isEmpty()) {
-        // 获取子图元中重合的属性，若子图元属性值不同，则值无效
-        result = result.insected(PageItem::attributions());
-
-        // 对于旋转属性，特殊处理，使用群组图元当前的角度值
-        auto itr = std::find_if(result.begin(), result.end(), [](const SAttri & attr) {
-            return bool(ERotProperty == attr.attri);
-        });
-        if (result.end() != itr) {
-            (*itr).var = drawRotation();
-        } else {
-            // 由直线组成的群组图元无旋转属性，单独添加旋转属性
-            result << SAttri(ERotProperty, drawRotation());
-        }
-    }
-
     return result;
 }
 
